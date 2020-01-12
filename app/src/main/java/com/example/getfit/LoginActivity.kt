36cd3ae3197c715f61c32a344deed1cfb52ad31f
kj7744 <50749738+kj7744@users.noreply.github.com>
2020-01-12 16:20:36 +0530

    database login activity

--- app/src/main/java/com/example/getfit/LoginActivity.kt
+++ app/src/main/java/com/example/getfit/LoginActivity.kt
@@ -3,7 +3,8 @@ package com.example.getfit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.util.Patterns
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_signup.*
@@ -19,22 +20,62 @@ class LoginActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 
         login_button.setOnClickListener {
-            startActivity(Intent(this , MainActivity::class.java))
-            finish()
+            dbLogin()
         }
 
-        signUp.setOnClickListener{
+        signUp_form.setOnClickListener{
             startActivity(Intent(this,signup::class.java))
             finish()
         }
     }
+
+    private fun dbLogin() {
+        if (!Patterns.EMAIL_ADDRESS.matcher(email1.text.toString()).matches()) {
+            email1.error = " Invalid email"
+            email1.requestFocus()
+            return
+        }
+        if (pass1.text.toString().isEmpty()) {
+            pass1.error = "password cannot be empty"
+            pass1.requestFocus()
+            return
+        }
+        auth.signInWithEmailAndPassword(email1.text.toString(), pass1.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                 //   Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                    Toast.makeText(baseContext, "Login successfull.",
+                        Toast.LENGTH_SHORT).show()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                  //  Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Login failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // ...
+            }
+
+    }
+
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         updateUI(currentUser)
     }
-    fun updateUI(currentUser: FirebaseUser?) {
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if(currentUser!=null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }else{
+            Toast.makeText(baseContext,"Login Failed",
+                Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
--- app/src/main/java/com/example/getfit/signup.kt
+++ app/src/main/java/com/example/getfit/signup.kt
@@ -7,9 +7,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_signup.*
-import kotlinx.android.synthetic.main.activity_signup.email
-import kotlinx.android.synthetic.main.activity_signup.pass
-import kotlinx.android.synthetic.main.login_activity.*
+
 
 class signup : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -18,7 +16,7 @@ class signup : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
         // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
-        signUp_form.setOnClickListener {
+        signUp_user.setOnClickListener {
             signupUser()
         }
 
@@ -26,24 +24,35 @@ class signup : AppCompatActivity() {
 
     private fun signupUser() {
         if (username.text.toString().isEmpty()) {
-            username.error = "user name is empty"
+            username.error = "user name cannot be  empty"
             username.requestFocus()
             return
         }
         if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
-            email.error = "user name is empty"
+            email.error = " Invalid email"
             email.requestFocus()
             return
         }
-        if (pass.text.toString().isEmpty()) {
-            username.error = "user name is empty"
-            username.requestFocus()
+        if (pass.text.toString().isEmpty() || pass.text.length < 6) {
+            pass.error = "password cannot be empty and less than 6 digit"
+            pass.requestFocus()
             return
         }
+//        val password: String = passwordText.getText().toString()
+//        if (password.isEmpty() || password.length < 6) {
+//            passwordText.setError("Password cannot be less than 6 characters!")
+//        } else {
+//            passwordText.setError(null)
+//            startActivity(Intent(this@RegistrationActivity, MainActivity::class.java))
+//        }
+
         auth.createUserWithEmailAndPassword(email.text.toString(), pass.text.toString())
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
+                    Toast.makeText(baseContext, "Sigup successfull.",
+                        Toast.LENGTH_SHORT).show()
                     startActivity(Intent(this, LoginActivity::class.java))
+
 
                 } else {
                     // If sign in fails, display a message to the user.

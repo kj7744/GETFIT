682b1a79ecc0dfdcaa5fa9b4d4839abe67951012
kj7744 <50749738+kj7744@users.noreply.github.com>
2020-01-15 00:27:30 +0530

    drawer layout

--- app/src/main/java/com/example/getfit/MainActivity.kt
+++ app/src/main/java/com/example/getfit/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.getfit
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
